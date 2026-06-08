718a8c921babeada0d01f00b077d1618244b0a6f
if-space <dima.shoto22@gmail.com>
2026-06-08 21:27:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lessons
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            int currentValue = GetIntegerFromUser();
+
+            Console.WriteLine($"Все успешно: {currentValue}");
+        }
+
+        static int GetIntegerFromUser()
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите целое число: ");
+
+                string userInput = Console.ReadLine();
+
+                if (int.TryParse(userInput, out int number))
+                    return number;
+                else
+                    Console.WriteLine("Введено некорректное число");
+            }
+        }
+    }
+}
